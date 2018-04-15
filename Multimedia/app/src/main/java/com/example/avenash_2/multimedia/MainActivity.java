8e5ed227b9179e7d5f064a41d298cc0325a5c162
AvenashKumar <avenash_kumar@hotmail.com>
2018-04-14 21:31:21 -0500

    Main activity layout created

---
+++ Multimedia/app/src/main/java/com/example/avenash_2/multimedia/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.avenash_2.multimedia;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onCamGallery(View view) {
+    }
+
+    public void onVideoRec(View view) {
+    }
+
+    public void onAudioRec(View view) {
+    }
+
+    public void onVideoView(View view) {
+    }
+}
